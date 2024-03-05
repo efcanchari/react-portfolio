7d971c744cba5d924a162722d7cc938d475a4619
E. Frank C. Lapa <frank.lapa@itexp.co.uk>
2024-03-05 22:50:17 +0000

    Add Project.css

--- src/Project.js
+++ src/Project.js
@@ -1,15 +1,21 @@
 import React, { Component } from 'react';
+import './Project.css';
 import projectLogo from './project-logo.png';
 
 class Project extends Component {
     render() {
-        const { title, link, repo, image } = this.props.details;
+        const { title, description, link, repo, image } = this.props.details;
         return (
-            <div>
-                <h2>{title}</h2>
-                <a href={link} target="_blank" >Deployed Version</a>
-                <a href={repo} target="_blank" >GitHub Repo</a>
-                <img src={projectLogo} alt={`Screenshot of ${title}`} />
+            <div className="project-container">
+                <div className="project-image">
+                    <img src={projectLogo} alt={`Screenshot of ${title}`}/>
+                </div>
+                <div className="project-info">
+                    <h2>{title}</h2>
+                    <p>{description}</p>
+                    Deployed Version: <a href={link} target="_blank" rel="noopener noreferrer">{link}</a><br/>
+                    GitHub Repo: <a href={repo} target="_blank" rel="noopener noreferrer">{repo}</a>
+                </div>
             </div>
         );
     }
