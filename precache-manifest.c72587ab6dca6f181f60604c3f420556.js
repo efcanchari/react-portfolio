5371c982ee3e7919ebceab23d4eb6a4149828d45
E. Frank C. Lapa <frank.lapa@itexp.co.uk>
2024-03-05 21:31:36 +0000

    Updates

---
+++ precache-manifest.c72587ab6dca6f181f60604c3f420556.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e47a7895c03b1ad16569cb2f3057f6d1",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "03245b4382e802c079c6",
+    "url": "/react-portfolio/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "605caa23b58ded0553e2",
+    "url": "/react-portfolio/static/js/2.3d1fc37f.chunk.js"
+  },
+  {
+    "revision": "184544feb51a777f05eb9d4b59832005",
+    "url": "/react-portfolio/static/js/2.3d1fc37f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1e1fd94d2c02dff5cf6",
+    "url": "/react-portfolio/static/js/3.1e3cddfe.chunk.js"
+  },
+  {
+    "revision": "03245b4382e802c079c6",
+    "url": "/react-portfolio/static/js/main.d8517db7.chunk.js"
+  },
+  {
+    "revision": "dbe01d23a2726aca6d4f",
+    "url": "/react-portfolio/static/js/runtime-main.f333f575.js"
+  },
+  {
+    "revision": "62766a364a7c9899b2384b1c533ef7a0",
+    "url": "/react-portfolio/static/media/frank-profile.62766a36.png"
+  }
+]);
